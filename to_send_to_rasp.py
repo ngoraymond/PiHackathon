b1867fdc80273d6fd96a06450a9e026df0b5b6d3
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2022-03-19 18:18:28 -0400

    added emailing functionality

--- to_send_to_rasp.py
+++ to_send_to_rasp.py
@@ -5,13 +5,33 @@ import os
 import cv2
 import pandas as pd
 
+import smtplib, ssl
+from email.message import EmailMessage
+
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
+def sendmail(dest, item, count, toSend = None):
+    msg = EmailMessage()
+    msg['Subject'] = f'{count} instances of {item} is missing'
+    msg['From'] = 'hackberrypi2022@gmail.com'
+    msg['To'] = dest
+
+    if toSend is not None:
+        cv2.imwrite(ROOT + 'to_send.jpg', toSend)
+        with open(ROOT + 'to_send.jpg', 'rb') as f:
+            file_data = f.read()
+            msg.add_attachment(file_data, maintype='image', subtype = 'jpg')
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = ssl.create_default_context()) as s:
+        s.login(msg['From'], 'password') #remember to place correct password
+        s.send_message(msg)
 
 if __name__ == '__main__':
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5m')  #one second for small, 3 seconds for medium?
+    input_email = input('Enter your email: ') #email to send to
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5m')  #one second for small, 5 seconds for medium
     webcam = cv2.VideoCapture(0)
     items_tracked = {}
+
 
     while True:
         #webcam data
@@ -39,8 +59,10 @@ if __name__ == '__main__':
         for item in items_tracked.keys():
             if item not in cur_seen:
                 print(f'{item} has left the camera view')
+                sendmail(input_email, item, 1, toSend = frame)
             elif items_tracked[item] > cur_seen[item]: #item has left the camera view
                 print(f'{items_tracked[item] - cur_seen[item]} instances of {item} has left the camera view')
+                sendmail(input_email, item, items_tracked[item] - cur_seen[item], toSend = frame)
             
         items_tracked = cur_seen
 
