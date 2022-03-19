a0d75f98f51c75fc2994173405c01db787ece6ad
ngoraymond <21043327+ngoraymond@users.noreply.github.com>
2022-03-19 11:16:51 -0400

    file rename

--- to_send_to_rasp.py
+++ to_send_to_rasp.py
@@ -2,13 +2,15 @@ import torch
 import sys
 import os
 import cv2
+import pandas as pd
 
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 if __name__ == '__main__':
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')  
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5m')  
     webcam = cv2.VideoCapture(0)
+    items_tracked = {}
 
     while True:
         #webcam data
@@ -21,3 +23,17 @@ if __name__ == '__main__':
         results = model(frame)
 
         results.print()
+
+        pds = results.pandas().xyxy[0]
+
+        #only get high confident estimate
+        pds = pds[pds.confidence > 0.5]
+
+        cur_seen = {}
+        for item in pds.name:
+            if item not in cur_seen:
+                cur_seen[item] = 0
+            cur_seen[item] = cur_seen[item] + 1
+
+        
+
